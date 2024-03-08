f60ce7eaf97e69d95af66e4725324b4c887fea97
unknown <shivamswami8899@gmail.com>
2024-03-08 11:29:50 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import logo from "./firebase.png";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { ganerateToken } from "./notification/firebase";
-import { getMessaging, onMessage } from "firebase/messaging";
+import { onMessage } from "firebase/messaging";
 import { messaging } from "./notification/firebase";
+
 function App() {
-  const [formData, setFormData] = useState({
+  const [formData1, setFormData1] = useState({
     title: "",
     body: "",
     image: "",
     token: "",
   });
 
-  const handleChange = (e) => {
+  const [formData2, setFormData2] = useState({
+    title: "",
+    body: "",
+    image: "",
+  });
+
+  const handleChangeForm1 = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
+    setFormData1((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
+
+  const handleChangeForm2 = (e) => {
+    const { name, value } = e.target;
+    setFormData2((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   const postNotification = async (formData) => {
     const obj = {
       title: formData.title,
@@ -27,13 +43,16 @@ function App() {
       token: formData.token,
     };
     try {
-      const res = await fetch("https://push-notifications-backend.onrender.com/notificationPost", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(obj),
-      });
+      const res = await fetch(
+        "https://push-notifications-backend.onrender.com/notificationPost",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(obj),
+        }
+      );
 
       if (!res.ok) {
         throw new Error("Network response was not ok");
@@ -46,15 +65,30 @@ function App() {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmitForm1 = (e) => {
     e.preventDefault();
-    postNotification(formData);
-    // setFormData({
-    //   title: "",
-    //   body: "",
-    //   image: "",
-    //   token: "",
-    // });
+    postNotification(formData1);
+  };
+
+  const getToken = async (formData2) => {
+    const res = await fetch(
+      "https://push-notifications-backend.onrender.com/token"
+    );
+    const data = await res.json();
+    data.map(async (el) => {
+      const newObj = {
+        title: formData2.title,
+        image: formData2.image,
+        body: formData2.body,
+        token: el.token,
+      };
+      await postNotification(newObj);
+    });
+  };
+
+  const handleSubmitForm2 = (e) => {
+    e.preventDefault();
+    getToken(formData2);
   };
 
   useEffect(() => {
@@ -79,45 +113,84 @@ function App() {
 
       <img src={logo} className="App-logo" alt="logo" />
 
-      <form onSubmit={handleSubmit}>
+      <div className="formDiv">
         <div>
-          <label>Title:</label>
-          <input
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
+          <h2>Form-1 With Token</h2>
+          <form onSubmit={handleSubmitForm1}>
+            <div>
+              <label>Title:</label>
+              <input
+                type="text"
+                name="title"
+                value={formData1.title}
+                onChange={handleChangeForm1}
+              />
+            </div>
+            <div>
+              <label>Body:</label>
+              <input
+                type="text"
+                name="body"
+                value={formData1.body}
+                onChange={handleChangeForm1}
+              />
+            </div>
+            <div>
+              <label>Image URL:</label>
+              <input
+                type="text"
+                name="image"
+                value={formData1.image}
+                onChange={handleChangeForm1}
+              />
+            </div>
+            <div>
+              <label>Token:</label>
+              <input
+                type="text"
+                name="token"
+                value={formData1.token}
+                onChange={handleChangeForm1}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
         <div>
-          <label>Body:</label>
-          <input
-            type="text"
-            name="body"
-            value={formData.body}
-            onChange={handleChange}
-          />
+          <h2>Form-2 Without Token</h2>
+          <form onSubmit={handleSubmitForm2}>
+            <div>
+              <label>Title:</label>
+              <input
+                type="text"
+                name="title"
+                value={formData2.title}
+                onChange={handleChangeForm2}
+              />
+            </div>
+            <div>
+              <label>Body:</label>
+              <input
+                type="text"
+                name="body"
+                value={formData2.body}
+                onChange={handleChangeForm2}
+              />
+            </div>
+            <div>
+              <label>Image URL:</label>
+              <input
+                type="text"
+                name="image"
+                value={formData2.image}
+                onChange={handleChangeForm2}
+              />
+            </div>
+
+            <button type="submit">Submit</button>
+          </form>
         </div>
-        <div>
-          <label>Image URL:</label>
-          <input
-            type="text"
-            name="image"
-            value={formData.image}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Token:</label>
-          <input
-            type="text"
-            name="token"
-            value={formData.token}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
   );
 }
